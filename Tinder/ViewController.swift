2f6a6f81efa595ffb8a1b11a60fbaecf78eecf22
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-02-24 16:27:14 +0900

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Kazutomo Kita on 2020/02/24.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
